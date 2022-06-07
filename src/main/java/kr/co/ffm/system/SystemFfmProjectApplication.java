29a6a4bb63bfc350578938c6a93ff5b9be919772
JuYoung <jfullover@gmail.com>
2022-06-07 13:13:59 +0900

    스케줄러 수정

--- src/main/java/kr/co/ffm/system/SystemFfmProjectApplication.java
+++ src/main/java/kr/co/ffm/system/SystemFfmProjectApplication.java
@@ -2,8 +2,10 @@ package kr.co.ffm.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SystemFfmProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/kr/co/ffm/system/feeding/FeedingScheduler.java
+++ src/main/java/kr/co/ffm/system/feeding/FeedingScheduler.java
@@ -1,9 +1,11 @@
 package kr.co.ffm.system.feeding;
 
+import kr.co.ffm.system.config.ApplicationContextProvider;
 import kr.co.ffm.system.farmedfish.FarmedFish;
 import kr.co.ffm.system.farmedfish.FarmedFishMapper;
 import kr.co.ffm.system.watertank.Watertank;
 import kr.co.ffm.system.watertank.WatertankMapper;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,6 +17,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Service
+@Log4j2
 public class FeedingScheduler {
     @Autowired
     private WatertankMapper watertankMapper;
@@ -22,46 +25,56 @@ public class FeedingScheduler {
     private FarmedFishMapper farmedFishMapper;
     @Autowired
     private FeedingService feedingService;
-    @Autowired
-    private FeedingMapper feedingMapper;
-    private Map<String, List<String>> sendTarget;
+//    @Autowired
+//    private FeedingMapper feedingMapper;
+//    private Map<String, List<String>> sendTarget;
+//
+//
+//    public FeedingScheduler() {
+//        List<Watertank> list = watertankMapper.selectAll(new Watertank());
+//
+//        FarmedFish farmedFish = null;
+//        for (Watertank watertank : list) {
+//            farmedFish = new FarmedFish();
+//            farmedFish.setNo(watertank.getFarmedFishNo());
+//            FarmedFish selectedFish = farmedFishMapper.selectByNo(farmedFish);
+//
+//            String[] feedingTimes = selectedFish.getFeedingTime().split(",");
+//            List<String> feedingTimeList = new ArrayList<>(Arrays.asList(feedingTimes));
+//
+//            sendTarget.put(watertank.getId(), feedingTimeList);
+//        }
+//    }
 
-    public FeedingScheduler() {
-        List<Watertank> list = watertankMapper.selectAll(new Watertank());
-
-        FarmedFish farmedFish = null;
-        for (Watertank watertank : list) {
-            farmedFish = new FarmedFish();
-            farmedFish.setNo(watertank.getFarmedFishNo());
-            FarmedFish selectedFish = farmedFishMapper.selectByNo(farmedFish);
-
-            String[] feedingTimes = selectedFish.getFeedingTime().split(",");
-            List<String> feedingTimeList = new ArrayList<>(Arrays.asList(feedingTimes));
-
-            sendTarget.put(watertank.getId(), feedingTimeList);
-        }
-    }
-
-    @Scheduled(initialDelay = 1000 * 65, fixedDelay = 1000 * 60)
+    @Scheduled(initialDelay = 1000 * 1, fixedDelay = 1000 * 10)
     public void autoSendData() {
+        log.info("작동중");
+
         LocalTime now = LocalTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         String formatedNow = now.format(formatter);
 
-        Iterator<String> keys = sendTarget.keySet().iterator();
-        String key = null;
+        List<Watertank> list = watertankMapper.selectAll(new Watertank());
 
-        while (keys.hasNext()) {
-            key = keys.next().toString();
+        FarmedFish farmedFish = null;
+        for (Watertank watertank : list) {
+            log.info("테스트");
+            farmedFish = new FarmedFish();
+            log.info(watertank);
+            farmedFish.setNo(watertank.getFarmedFishNo());
 
-            List<String> feedingTimes = sendTarget.get(key);
+            FarmedFishMapper farmedFishMapper = ApplicationContextProvider.getBean(FarmedFishMapper.class);
+            FarmedFish selectedFish = farmedFishMapper.selectByNo(farmedFish);
+            log.info(farmedFishMapper.selectByNo(farmedFish));
+            String[] feedingTimes = selectedFish.getFeedingTime().split(",");
             for (String time : feedingTimes) {
                 if (time.equals(formatedNow)) {
-                    Watertank watertank = new Watertank();
-                    watertank.setId(key);
+                    log.info("--밥줌--");
                     feedingService.sendFeeding(watertank);
                 }
+                log.info("--안줌--");
             }
         }
+
     }
 }
